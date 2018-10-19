200317217ac996dd01b9cc8a365d61b5edc9479c
ilkerbey-01 <34978332+ilkerbey-01@users.noreply.github.com>
2018-10-19 18:53:22 +0300

    multicommand system making has started

--- bot.js
+++ bot.js
@@ -14,6 +14,72 @@ const log = message => {
 ////////////
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+fs.readdir('./cmd-multilanguage/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./cmd-multilanguage/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./cmd-multilanguage/${command}`)];
+            let cmd = require(`./cmd-multilanguage/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./cmd-multilanguage/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./cmd-multilanguage/${command}`)];
+            let cmd = require(`./cmd-multilanguage/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+////////////
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 fs.readdir('./cmd-tr/', (err, files) => {
   if (err) console.error(err);
   log(`${files.length} komut yüklenecek.`);
